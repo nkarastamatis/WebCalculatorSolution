cc2172d19f6f09ecf678700ae4ed060c179e2e57
Nick Karastamatis <nkarastamatis@gmail.com>
2013-09-20 23:53:36 -0700

    Ch 2 Practice exercises

---
+++ WebCalculator/CalculatorLibrary.js
@@ -0,0 +1,42 @@
+﻿var txtInput;
+var txtResult;
+
+function initialize() {
+    txtInput = document.getElementById('txtInput');
+    txtResult = document.getElementById('txtResult');
+
+    document.getElementById('btnPlus').addEventListener('click', plusClick, false);
+    document.getElementById('btnMinus').addEventListener('click', minusClick, false);
+    document.getElementById('btnClearEntry').addEventListener('click', clearEntry, false);
+    document.getElementById('btnClear').addEventListener('click', clear, false);
+
+    for (var i = 0; i < 10; i++) {
+        document.getElementById('btn' + i).addEventListener('click', numberClick, false);
+    }
+
+    clear();
+}
+
+function numberClick() {
+    txtInput.value = txtInput.value == '0' ?
+        this.innerText : txtInput.value + this.innerText;
+}
+
+function plusClick() {
+    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
+    clearEntry();
+}
+
+function minusClick() {
+    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
+    clearEntry();
+}
+
+function clearEntry() {
+    txtInput.value = '0';
+}
+
+function clear() {
+    txtInput.value = '0';
+    txtResult.value = '0';
+}
